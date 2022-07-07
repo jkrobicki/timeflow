2567b4eee31ca186f163e432d9411990c655c233
liberty-rising <rwyszomi@gmail.com>
2022-07-07 14:30:52 +0200

    modify user dropdown on capacity page to read results alphabetically

--- applications/timeflow/data/users.py
+++ applications/timeflow/data/users.py
@@ -124,29 +124,33 @@ def users_names(is_active: bool = None, label="select user") -> List[Select]:
     api = f"{base_url}/api/users"
     params = {"is_active": is_active}
     response = requests.get(api, params=params)
-    rows = [Select(value="", display_value=label)]
+    label = [Select(value="", display_value=label)]
+    rows = []
     for item in response.json():
         d = Select(
             value=item["id"],
             display_value=(item["last_name"] + " " + item["first_name"]),
         )
         rows.append(d)
+    rows = sorted(rows[1:], key=lambda d: d["display_value"])
+    rows = label + rows
     return rows
 
 
 def users_names_str(is_active: bool = None, label="select user") -> List[Select]:
     # Connect to users list endpoint and return value as name
     api = f"{base_url}/api/users"
-    params = {"is_active": is_active}
+    params = {"is_active": True}
     response = requests.get(api, params=params)
-    rows = [Select(value="", display_value=label)]
+    rows = [Select(value="", display_value="select user")]
     for item in response.json():
         d = Select(
-            value=item["last_name"] + " " + item["first_name"],
+            value=(item["last_name"] + " " + item["first_name"]),
             display_value=(item["last_name"] + " " + item["first_name"]),
         )
         rows.append(d)
     return rows
+
 
 def user_full_name_by_id(user_id):
     api = f"{base_url}/api/users/{user_id}"
