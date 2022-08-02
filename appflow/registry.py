9ec758695072745b0b85d71d3cdf7cc6146f295c
jkrobicki <jkrobickir@dyvenia.com>
2022-08-02 10:36:48 +0200

    removed idom

--- appflow/registry.py
+++
@@ -1,5 +0,0 @@
-from idom.backend import fastapi
-from idom.backend.starlette import Options
-from index import page as index_page
-
-fastapi.configure(app, index_page, Options(redirect_root=False, url_prefix="/_idom"))
