e625c677ced09d66860f8e83a694f19ca8344bfc
krobicki.jedrzej@gmail.com <jedrzej@LAPTOP-23USUFCV.localdomain>
2022-08-22 10:43:02 +0200

    updated main timeflow changes

--- backend/api/capacity.py
+++ backend/api/capacity.py
@@ -64,13 +64,12 @@ async def get_capacities(
         Year of capacity in question.
     """
     statement = select(
-        Capacity.id,
+        Capacity.id.label("capacity_id"),
         AppUser.last_name,
         AppUser.first_name,
         Capacity.year,
         Capacity.month,
         Capacity.days,
-        (AppUser.first_name + ' ' + AppUser.last_name).label("full_name"),
     ).join(AppUser, Capacity.user_id == AppUser.id)
     if (user_id and month and year) != None:
         statement_final = (
