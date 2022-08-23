6ed312ec57e30c6ae36c4a3187af7375667269b1
krobicki.jedrzej@gmail.com <jedrzej@LAPTOP-23USUFCV.localdomain>
2022-08-23 13:10:38 +0200

    added dateTimePicker component

---
+++ frontend/src/library/components/dateTimePicker.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let value = 'test';
+	async function onBlur(e) {
+		let inputValue = e.target.value;
+		value = inputValue.replace('T', ' ');
+	}
+</script>
+
+<input type="datetime-local" class="month-picker" on:blur={(e) => onBlur(e)} />
