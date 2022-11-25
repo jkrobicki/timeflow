93946bbd7e2351d8a422fdef5a869590e6191b83
jkrobicki <krobicki.jedrzej@gmail.com>
2022-11-25 18:24:26 +0100

    updated token /post endpoint

--- backend/api/token.py
+++ backend/api/token.py
@@ -5,37 +5,62 @@ from pydantic import BaseModel
 import requests
 import json
 import requests
-from datetime import datetime
+from datetime import datetime, date
+
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 
 from ..utils import engine, get_session
 from ..models.user import AppUser
 from .user import post_user
 
+from jose import JWTError, jwt
+import json
+
 router = APIRouter(prefix="/api/token", tags=["token"])
+
+SECRET_KEY = "5556ed7886a0f5a1e0aa2aeb30b40191"
+ALGORITHM = "HS256"
 
 
 class Token(BaseModel):
     value: str
 
 
-@router.post("/")
-async def test(token: Token, session: Session = Depends(get_session)):
-    headers = {"Authorization": f"bearer {token.value}"}
-    response = requests.get(
-        "http://172.28.1.11:8080/realms/master/protocol/openid-connect/userinfo",
-        headers=headers,
-    ).json()
-    # need to try except this
-    name = response["given_name"]
-    last_name = response["family_name"]
-    username = response["preferred_username"]
-    email = response["email"]
+class User(BaseModel):
+    username: str
+    name: str
+    lastname: str
+    email: str
 
-    user = AppUser(
-        username=username,
-        first_name=name,
-        last_name=last_name,
-        email=email,
+
+@router.post("/")
+async def test(user: User, session: Session = Depends(get_session)):
+    payload = {
+        "name": user.name,
+        "lastname": user.lastname,
+        "email": user.email,
+        "username": user.username,
+    }
+    token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+    # headers = {"Authorization": f"bearer {token.value}"}
+    # response = requests.get(
+    #     "http://172.28.1.11:8080/realms/master/protocol/openid-connect/userinfo",
+    #     headers=headers,
+    # ).json()
+    # # need to try except this
+    # name = response["given_name"]
+    # last_name = response["family_name"]
+    # username = response["preferred_username"]
+    # email = response["email"]
+
+    post_user = AppUser(
+        username=user.username,
+        first_name=user.name,
+        last_name=user.lastname,
+        email=user.email,
+        role_id=0,
+        start_date=date.today(),
         created_at=datetime.now(),
         updated_at=datetime.now(),
         is_active=True,
@@ -44,11 +69,8 @@ async def test(token: Token, session: Session = Depends(get_session)):
     statement = select(AppUser).where(AppUser.username == user.username)
     try:
         result = session.exec(statement).one()
-        return False
     except NoResultFound:
-        session.add(user)
+        session.add(post_user)
         session.commit()
-        session.refresh(user)
-        return user
-
-    return {"res": response.json(), "headers": headers, "result": result}
+        session.refresh(post_user)
+    return {"access_token": token, "user": payload}
